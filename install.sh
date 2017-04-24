dac93c4c00e8df5c543dc4eb5599f422a6a90c4d
Christopher Reighley <reighley@gmail.com>
2017-04-24 17:26:23 +0000

    #39 (part 1) yum setup for new server

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-cp services/run_master/src/pipefitter/pipefitter /nfs/export/UTIL/bin
+#yum install erlang pygpgme yum-utils nginx
+#cp etc/yum.repos.d/* /etc/yum.repos.d
+#yum -q makecache -y
+
+#yum install rabbitmq-server
+
+#yum install riak --nogpgcheck
+
+#mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
+#cp etc/nginx/nginx.conf /etc/nginx/nginx.conf
+
+#cp -r www /www
+
+#cp services/run_master/src/pipefitter/pipefitter /nfs/export/UTIL/bin
